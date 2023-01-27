a35eec4fb1fe71edba53fff4c724c06f866ce7e9
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-27 16:38:13 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,68 @@
+export const item = {
+    "id": "e1ae2ffa-d647-4740-9a46-0a4c792df6ed",
+    "food_list": [
+        {
+            "id": "f96db9f9-1972-497d-b1ce-b010ab44ccd3",
+            "food_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/266.png"
+        },
+        {
+            "id": "795588f3-fabd-4c69-b98a-6c3d6c645456",
+            "food_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+        },
+        {
+            "id": "788ba9ef-6573-4439-8f17-d919170a365c",
+            "food_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png"
+        },
+        {
+            "id": "b0f4ba67-86ab-4a93-8284-7ab05fadc5c1",
+            "food_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+        },
+        {
+            "id": "a3cf50d1-60f5-43a3-99f6-90177f6e00e0",
+            "food_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+        }
+    ],
+    "camera_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png",
+    "add_photos": "continue",
+    "food_taste": "Hospital task leader carry key force. Themselves day others under drive animal.",
+    "reviews": "officer",
+    "review_count": "weight",
+    "restaurant_count": "In cup foot idea western mother.",
+    "traveler_rating_title": "power",
+    "rating_type": "bed",
+    "rating_count": 9137,
+    "rating_type2": "test",
+    "rating_count2": 164,
+    "rating_type3": "myself",
+    "rating_count3": 127,
+    "rating_type4": "same",
+    "rating_count4": 1086,
+    "popular_mentions": "raise",
+    "mentions_list": [
+        {
+            "id": "3a37c9d9-686f-4689-b783-a69511e57b25",
+            "mention_title": "civil",
+            "review_count": "ready"
+        },
+        {
+            "id": "a84d490f-5465-4f59-acc6-3cebf4d3178c",
+            "mention_title": "begin",
+            "review_count": "sister"
+        },
+        {
+            "id": "12fa7158-c0a5-4bea-865a-070540f2a92e",
+            "mention_title": "happy",
+            "review_count": "his"
+        },
+        {
+            "id": "a2f5d06c-9409-4c1d-a47b-cfc9fa543032",
+            "mention_title": "ground",
+            "review_count": "least"
+        },
+        {
+            "id": "5d5d33a4-8a1b-4403-a9a5-72bec47a12f5",
+            "mention_title": "find",
+            "review_count": "material"
+        }
+    ]
+}
